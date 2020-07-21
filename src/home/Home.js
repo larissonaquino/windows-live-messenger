a793767749282edbd8be578b45651488c3a9d575
Lárisson Aquino <larissonaquino@gmail.com>
2020-07-20 23:12:05 -0300

    list users from API and screen adjustments

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import api from '../services/api'
 import './Home.css'
 
-function Home(userData) {
+function Home(data) {
     const [userName, setUserName] = useState('');
     const [subnick, setSubnick] = useState('');
     const [users, setUsers] = useState([]);
 
-    api.get('/users').then((usuarios) => {
-        setUsers(usuarios.data)
-        console.log('user', usuarios);
-    });
+    useEffect(() => {
+        async function getUsers() {
+            await api.get('/users').then((usuarios) => {
+                setUsers(usuarios.data)
+                console.log('user', usuarios);
+            })
+            .catch(e => {
+                console.error('Não foi possível se conectar ao servidor:', e)
+            });
+        }
+
+        getUsers();
+    }, []);
 
     function updateSubnick(description) {
         setSubnick(description);
@@ -20,6 +29,9 @@ function Home(userData) {
         api.post('/updateSubnick', {
             email,
             subnick
+        })
+        .catch(e => {
+            console.error('Não foi possível se conectar ao servidor:', e)
         });
     }
 
@@ -30,19 +42,26 @@ function Home(userData) {
                     <img src="https://avatars2.githubusercontent.com/u/29270648?s=400&u=33e774e5958640ad496d1ebf5a74e8204f2d54f6&v=4" alt="Eu"/>
                     <div className="signature">
                         <span className="user-name">Lárisson Aquino <span className="status-title">(Available)</span></span>
-                        <span className="statusMessage">If you can see my heart, then you already know I'm falling apart</span>
+                        <input className="statusMessage" onBlur={e => updateSubnick(e.target.value)} />
                     </div>
                 </div>
                 <div className="search-session">
-                    <input id="search-user" placeholder="Describe your subnick here" onBlur={e => updateSubnick(e.target.value)}></input>
+                    <input id="search-user" placeholder="Search user here" />
                 </div>
             </header>
             <main>
                 <div className="user-list">
-                    <div display="block">
-                        <span className="user-list-name">Lárisson</span>
-                        <span className="user-list-subnick">- {subnick}</span>
-                    </div>
+                    { users.map(user => {
+                        return (
+                            <ul key={ user._id } className="user-data">
+                                <li>
+                                    <span className="user-list-name">{ user.name }</span>
+                                    <span className="user-list-subnick">{ user.subnick ? ' - ' + user.subnick : '' }</span>
+                                </li>
+                            </ul>
+                            )
+                        }) 
+                    }
                 </div>
             </main>
         </div>
